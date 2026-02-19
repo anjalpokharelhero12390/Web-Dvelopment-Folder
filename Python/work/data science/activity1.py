8fd3c2c28e8f01bc037437d042a51fb97e3cf70d
anjalpokharelhero12390 <anjalpokharel12390@gmail.com>
2026-02-19 20:44:50 +0545

    activity1.commit

---
+++ Python/work/data science/activity1.py
@@ -0,0 +1,26 @@
+import numpy as np
+a = np.arange(9, dtype=np.float_).reshape(3,3)
+print('First array:')
+print(a)
+print('\n')
+b = np.array([10,10,10])
+print('Second array:')
+print(b)
+print('\n')
+print('Add two arrays:')
+print(np.add(a,b))
+print('\n')
+print('Subtract two arrays:')
+print(np.subtract(a,b))
+print('\n')
+print('Multiply two arrays:')
+print(np.multiply(a,b))
+print('\n')
+print('Divide two arrays:')
+print(np.divide(a,b))
+print('\n')
+
+
+
+
+
